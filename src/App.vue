0b2cbaaf35378e900da01ef54fa616da4c863d02
Renzo Alves <djenrolandy@gmail.com>
2022-08-25 08:11:56 -0300

    aula-1 <Ajustes>

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
     <div class="px-3 py-10 md:px-10">
         <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
-
             <todo-spinner v-if="loading" />
             <template v-else>
-                
                 <todo-form-add />
 
                 <todo-items />
 
                 <todo-empty />
-
             </template>
-
         </div>
     </div>
 </template>
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
-import { createStore } from 'vuex'
+import {
+  createStore
+} from 'vuex'
 
 export default createStore({
   state: {
@@ -6,16 +8,18 @@ export default createStore({
   },
 
   mutations: {
-    storeTodos( state, payload){
+    /**
+     * 
+     * @param {string} state A fonte de dados.
+     * @param {data} payload Os dados recebidos para update.
+     */
+    storeTodos(state, payload) {
       state.todos = payload
 
     }
   },
 
-  getters: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  getters: {},
+  actions: {},
+  modules: {}
 })
